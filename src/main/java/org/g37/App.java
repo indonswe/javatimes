eead200acf7b89ed9cbf3e96041548ccd7d0e055
indonswe <afc2002afc@hotmail.com>
2021-06-27 12:01:38 +0200

    Exercise 3 finished

--- src/main/java/org/g37/App.java
+++ src/main/java/org/g37/App.java
@@ -9,12 +9,19 @@ import java.time.format.DateTimeFormatter;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         System.out.println(LocalDate.now());
 
         LocalDate march29 = LocalDate.parse("2020-03-25");
-
         System.out.println(march29.format(DateTimeFormatter.ofPattern("eeee dd MMM")));
+
+
+        LocalDate weekDay = LocalDate.parse("2021-06-21");
+        Boolean finished;
+        do {
+            System.out.println(weekDay);
+            weekDay = weekDay.plusDays(1);
+            finished=weekDay.isEqual(LocalDate.parse("2021-06-28"));
+        } while (!finished);
     }
 }
